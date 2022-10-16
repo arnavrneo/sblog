faf7e79e2515e457c9ca8e1555fda25a5e160b11
Arnav <48650781+arnavrneo@users.noreply.github.com>
2022-10-16 21:44:52 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,7 +48,7 @@
 				<span
 					class="relative inline-block ml-2 before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 "
 				>
-					<span class="relative text-slate-200 skew-y-3 skew-y-3">{SITE_TITLE}</span>
+					<span class="relative text-slate-100 skew-y-3 skew-y-3">{SITE_TITLE}</span>
 				</span>
 				!
 			</h1>
