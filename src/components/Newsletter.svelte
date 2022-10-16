12a0340c093ba5276c81d8bd667224d5367830ec
Arnav <48650781+arnavrneo@users.noreply.github.com>
2022-10-16 21:09:24 +0530

    Update Newsletter.svelte

--- src/components/Newsletter.svelte
+++ src/components/Newsletter.svelte
@@ -12,7 +12,7 @@
 
 <section class="mb-16 w-full" id="newsletter">
 	<div
-		class="my-4 w-full border-y border-blue-200 bg-blue-50 p-6 dark:border-gray-600 dark:bg-gray-800 sm:rounded sm:border-x"
+		class="my-4 w-full border-y border-blue-200 bg-blue-50 p-6 dark:border-gray-600 dark:bg-neutral-800 sm:rounded sm:border-x"
 	>
 		<div class="flex items-center justify-between space-x-4 text-gray-900 dark:text-gray-100">
 			<p class="text-lg font-bold md:text-xl">Something here</p>
