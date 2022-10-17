54f1626d72fd30fa66588dcf8c96ec1e4bdceea5
Arnav <48650781+arnavrneo@users.noreply.github.com>
2022-10-17 23:49:05 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -53,7 +53,7 @@
 				</span>
 				!
 			</h1>
-				<img src="/logo.png" class="rounded-full h-20 w-20 hidden md:h-36 w-36" alt="Avatar">
+				<img src="/logo.png" class="rounded-full h-20 w-20 md:h-36 w-36" alt="Avatar">
 			</div>
 			<h2 class="mb-4 text-gray-700 dark:text-gray-200">
 				write what you want to write.
