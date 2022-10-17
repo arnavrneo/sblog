5f0d362525e9b3eaee56a3d7d8d95581760fbf39
Arnav <48650781+arnavrneo@users.noreply.github.com>
2022-10-17 23:18:47 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,7 +42,7 @@
 >
 	<div class="flex flex-col-reverse items-start sm:flex-row">
 		<div class="flex flex-col pr-8">
-			<div class="flex flex-col justify-center">
+			<div class="flex flex-row align-center justify-center">
 			<h1 class="mb-3 text-3xl font-montserrat font-bold tracking-tight text-black dark:text-white md:text-5xl">
 				This is
 
