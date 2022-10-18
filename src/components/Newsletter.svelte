3ecc8b7854c3a1f396634dfbed85ab960bd8581f
Arnav <48650781+arnavrneo@users.noreply.github.com>
2022-10-18 18:58:15 +0530

    Update Newsletter.svelte

--- src/components/Newsletter.svelte
+++ src/components/Newsletter.svelte
@@ -12,25 +12,51 @@
 <!-- my-4 w-full border border-blue-200 bg-blue-50 p-6 dark:border-gray-600 dark:bg-neutral-600 sm:rounded sm:border-x -->
 <section class="mb-16 w-full" id="newsletter">
 	<div
-		class="my-4 w-full rounded-xl ring-4 ring-[#a1641d] p-6 dark:ring-4 ring-[#f37c30] sm:rounded sm:border"
+		class="my-4 w-full rounded-xl ring-2 ring-[#a1641d] p-6 dark:ring-2 ring-[#f37c30] sm:rounded sm:border"
 	>
 		<div class="flex items-center justify-between space-x-4 text-gray-900 dark:text-gray-100">
-			<p class="text-lg font-bold md:text-xl">checksomething should be here</p>
+			<p class="text-lg font-bold md:text-xl">Wanna contribute?</p>
+			<button
+				aria-label="Toggle CTA"
+				class="flex h-9 w-9  items-center justify-center rounded-lg ring-gray-300 transition-all hover:ring-2"
+				on:click={toggleNewsletter}
+			>
+				{#if isNewsletterOpen}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				{:else}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M15.707 4.293a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 011.414-1.414L10 8.586l4.293-4.293a1 1 0 011.414 0zm0 6a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 111.414-1.414L10 14.586l4.293-4.293a1 1 0 011.414 0z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				{/if}
+			</button>
 		</div>
 		{#if isNewsletterOpen}
-<!-- 			<form class="relative my-4" on:submit={onSubmit}>
-				<input
-					type="email"
-					aria-label="Email for newsletter"
-					placeholder="tim@apple.com"
-					autocomplete="email"
-					required={true}
-					class="mt-1 block w-full rounded-md border-gray-300 bg-white px-4 py-2 pr-32 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:text-gray-100"
-				/><button
-					class="absolute right-1 top-1 flex h-8 w-28 items-center justify-center rounded bg-gray-100 px-4 pt-1 font-medium text-gray-900 dark:bg-gray-700 dark:text-gray-100"
-					type="submit">Subscribe</button
+			<form class="relative my-4" on:submit={onSubmit}>
+				<p
+					class="items-center justify-center rounded bg-gray-100 px-4 pt-1 font-medium text-gray-900 dark:bg-gray-700 dark:text-gray-100"
+					type="submit">Check out the Github to contribute articles!</p
 				>
-			</form> -->
+			</form>
 <!-- 			<p class="text-sm text-gray-800 dark:text-gray-200">
 				3 subscribers including my Mom – <a href="/#newsletter">23 issues</a>
 			</p> -->
